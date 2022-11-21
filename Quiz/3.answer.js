1113cb7daf2dfb1d3c68ec31a5a54eeeb6066b5c
mason park <joung4342@gmail.com>
2022-11-21 15:41:53 +0900

    hash answser

---
+++ Quiz/3.answer.js
@@ -0,0 +1,31 @@
+// 1. 같은 장르끼리 묶는다.
+// 2, 묶인 노래들을 재생 수능로 정렬을 해야 된다.
+// 3. 노래를 2개까지 자르는 작업을 해야한다.
+
+// 핵심 키워드는 묶는 것 과 정렬
+
+function solution(genres,plays) {
+    const genreMap = new Map();
+    genres.map((genre, idx) => [genre, plays[idx]])
+        .forEach(([genre,play],idx) => {
+            const data = genreMap.get(genre) || {total: 0, songs: []};
+            genreMap.set(genre, {
+                total : data.total + play,
+                songs: [...data.songs, {play, idx}]
+                    .sort((a, b) => b.play - a.play)
+                    .slice(0, 2)
+            });
+        })
+/*    console.log([...genreMap.entries()]
+        .sort((a, b) => b[1].total - a[1].total)
+        .flatMap((item => item[1].songs))
+        .map((item) => item.idx)
+    );*/
+    return [...genreMap.entries()]
+        .sort((a, b) => b[1].total - a[1].total)
+        .flatMap((item => item[1].songs))
+        .map((item) => item.idx);
+}
+
+
+console.log(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]));

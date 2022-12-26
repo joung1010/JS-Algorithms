450454745a608553da6af8be419b52b17a14771c
mason park <joung4342@gmail.com>
2022-12-26 21:19:44 +0900

    순열 조합 차이점 추가

--- Quiz/10.quiz.js
+++ Quiz/10.quiz.js
@@ -20,8 +20,59 @@ function combinations(arr, n) {
 }
 
 
+function permutations(arr, n) {
+    if (n === 1) return arr.map(v => [v]);
+    let result = [];
+
+    arr.forEach((fixed, idx, arr) => {
+        const rest = arr.filter((_, index) => index !== idx);
+        const perms = permutations(rest, n - 1);
+        const combine = perms.map((v) => [fixed, ...v]);
+        result.push(...combine);
+    });
+    return result;
+}
+
+
+
 function solution(numbers) {
     return [...new Set(combinations(numbers, 2).map(item => item[0] + item[1]))].sort((a, b) => a - b);
 }
 
+
+function solution2(numbers) {
+    return permutations(numbers, 2);
+}
+/*
+*순열 [
+  [ 2, 1 ], [ 2, 3 ], [ 2, 4 ],
+  [ 2, 1 ], [ 1, 2 ], [ 1, 3 ],
+  [ 1, 4 ], [ 1, 1 ], [ 3, 2 ],
+  [ 3, 1 ], [ 3, 4 ], [ 3, 1 ],
+  [ 4, 2 ], [ 4, 1 ], [ 4, 3 ],
+  [ 4, 1 ], [ 1, 2 ], [ 1, 1 ],
+  [ 1, 3 ], [ 1, 4 ]
+
+*
+*
+* */
+
+
+function solution3(numbers) {
+    return combinations(numbers, 2);
+}
+/*
+* 조합 [
+  [ 2, 1 ], [ 2, 3 ],
+  [ 2, 4 ], [ 2, 1 ],
+  [ 1, 3 ], [ 1, 4 ],
+  [ 1, 1 ], [ 3, 4 ],
+  [ 3, 1 ], [ 4, 1 ]
+]
+
+*
+*
+* */
 console.log(solution([2, 1, 3, 4, 1]));
+console.log('순열',solution2([2, 1, 3, 4, 1])); // 한번씩 반복
+console.log('조합',solution3([2, 1, 3, 4, 1]));// 한번 순회한곳은 다시 순회하지 않음

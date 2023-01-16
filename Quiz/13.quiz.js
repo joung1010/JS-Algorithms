19a6e47dde1d5b05b17355e4a71e42ec0ffe69d6
mason park <joung4342@gmail.com>
2023-01-16 20:13:57 +0900

    이중 포인터 quiz3

--- Quiz/13.quiz.js
+++ Quiz/13.quiz.js
@@ -78,7 +78,7 @@ function solution(gems) {
     while (str < gemsLength && end < gemsLength) {
         // 즉 구간합의 기준은 gemmKinds 이다
         if (collect.size === gemmKinds) {
-            if (end + str < answer[1] + answer[0]) {
+            if (end - str < answer[1] - answer[0]) {
                 answer = [str + 1, end + 1]; // 더 적은 값의 진열대로 갱신
             }
             const gem = gems[str];
@@ -95,40 +95,6 @@ function solution(gems) {
     }
     return answer;
 }
-/*테스트 1 〉	통과 (0.29ms, 33.4MB)
-테스트 2 〉	실패 (0.29ms, 33.4MB)
-테스트 3 〉	실패 (0.36ms, 33.6MB)
-테스트 4 〉	통과 (0.45ms, 33.3MB)
-테스트 5 〉	통과 (0.55ms, 33.5MB)
-테스트 6 〉	통과 (0.11ms, 33.4MB)
-테스트 7 〉	실패 (0.26ms, 33.7MB)
-테스트 8 〉	실패 (0.34ms, 33.5MB)
-테스트 9 〉	실패 (0.69ms, 33.6MB)
-테스트 10 〉	통과 (0.46ms, 33.5MB)
-테스트 11 〉	통과 (0.62ms, 33.6MB)
-테스트 12 〉	실패 (0.67ms, 33.5MB)
-테스트 13 〉	실패 (1.22ms, 33.5MB)
-테스트 14 〉	통과 (0.68ms, 33.6MB)
-테스트 15 〉	실패 (7.50ms, 36.8MB)
-효율성  테스트
-테스트 1 〉	실패 (5.02ms, 36.9MB)
-테스트 2 〉	실패 (4.14ms, 37.3MB)
-테스트 3 〉	실패 (5.87ms, 37.3MB)
-테스트 4 〉	통과 (4.38ms, 38.9MB)
-테스트 5 〉	실패 (7.17ms, 38MB)
-테스트 6 〉	실패 (7.37ms, 38MB)
-테스트 7 〉	실패 (7.62ms, 38.6MB)
-테스트 8 〉	실패 (9.16ms, 39MB)
-테스트 9 〉	실패 (10.17ms, 38.9MB)
-테스트 10 〉	실패 (10.00ms, 39.2MB)
-테스트 11 〉	실패 (13.63ms, 40.1MB)
-테스트 12 〉	통과 (12.82ms, 42MB)
-테스트 13 〉	실패 (15.53ms, 43.4MB)
-테스트 14 〉	실패 (16.99ms, 39.4MB)
-테스트 15 〉	통과 (16.77ms, 40.4MB)
-*
-*
-* */
 
 
 console.log(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]));

e0a8451c4938f7bd5c74019012321e6c05d0f3ee
mason park <joung4342@gmail.com>
2023-01-16 20:14:28 +0900

    이중 포인터 quiz3

--- Quiz/13.quiz.js
+++ Quiz/13.quiz.js
@@ -95,7 +95,22 @@ function solution(gems) {
     }
     return answer;
 }
+function solution2(gems) {
+    const cnt = new Set(gems).size;
+    const gemMap = new Map();
 
+    var answer = [1, gems.length];
+
+    gems.forEach((gem, i) => {
+        gemMap.delete(gem);
+        gemMap.set(gem, i);
+        if (gemMap.size === cnt){
+            const cand = [gemMap.values().next().value + 1, i + 1];
+            answer = answer[1] - answer[0] > cand[1] - cand[0] ? cand : answer;
+        }
+    })
+    return answer;
+}
 
 console.log(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]));
 console.log(solution(["AA", "AB", "AC", "AA", "AC"]));

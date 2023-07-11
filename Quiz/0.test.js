88dfc606dd9e3983ce42e37308de132064f7458d
mason park <joung4342@gmail.com>
2023-07-11 21:06:10 +0900

    [1차] 프렌즈4블록 문제 풀이중2

--- Quiz/0.test.js
+++ Quiz/0.test.js
@@ -3,14 +3,14 @@ function solution(m, n, board) {
     for (let i = 0; i < m; i++) {
         findBingo(gameBoard, n, m);
     }
-    return gameBoard.flatMap(v=>v).filter(v => !v).length
+    return gameBoard.flatMap(v => v).filter(v => !v).length
 }
 
 const findBingo = (board, width, height) => {
     const binggo = Array.from({length: height}, () => Array(height).fill(false));
-    for (let i = 1; i < height ; i++) {
+    for (let i = 1; i < height; i++) {
         for (let j = 1; j < width; j++) {
-            if(!board[i][j] || !board[i][j - 1] || !board[i - 1][j - 1] || !board[i - 1][j]) continue;
+            if (!board[i][j] || !board[i][j - 1] || !board[i - 1][j - 1] || !board[i - 1][j]) continue;
             if (board[i][j] === board[i][j - 1] && board[i][j] === board[i - 1][j - 1] && board[i][j] === board[i - 1][j]) {
                 binggo[i][j] = true;
                 binggo[i][j - 1] = true;
@@ -32,15 +32,22 @@ const makeBoard = (board, binggo) => {
         }
     });
     for (let i = 0; i < board.length - 1; i++) {
+        // null 처리 후에 한칸 식 이동하는 것이 아니라 전부 옮겨 보자
         board[i].forEach((item, idx) => {
             if (!item) return;
-            if(!board[i + 1][idx]) [board[i + 1][idx], board[i][idx]] = [item, null];
+            if (!board[i + 1][idx]) [board[i + 1][idx], board[i][idx]] = [item, null];
         });
     }
 };
 
-console.log(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]));
+// console.log(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]));
 // console.log(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]));
+console.log(solution(5, 6, ["AAAAAA",
+    "BBAATB",
+    "BBAATB",
+    "JJJTAA",
+    "JJJTAA"]));
+
 
 // 채점 결과
 // 정확성: 81.8

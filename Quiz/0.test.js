30ce516e94ac0475e4c51b72d656b1785da7eaec
mason park <joung4342@gmail.com>
2023-07-17 21:31:05 +0900

    [3차] 방금그곡 문제풀이중1

--- Quiz/0.test.js
+++ Quiz/0.test.js
@@ -1,54 +1,53 @@
-function solution(m, n, board) {
-    const gameBoard = Array.from({length: m}, (_, i) => [...board[i]]);
-    for (let i = 0; i < m; i++) {
-        findBingo(gameBoard, n, m);
+// 라디오에서 방금 나왔던 음악이 무슨 음악인지 궁금해질 때가 많다
+// 네오는 자신이 기억한 멜로디를 가지고 방금그곡을 이용해 음악을 찾는다
+// 디오 방송에서는 한 음악을 반복해서 재생할 때도 있어서 네오가 기억하고 있는
+// 멜로디는 음악 끝부분과 처음 부분이 이어서 재생된 멜로디일 수도 있다.
+// 반대로, 한 음악을 중간에 끊을 경우 원본 음악에는 네오가 기억한 멜로디가 들어있다 해도 그 곡이 네오가 들은 곡이 아닐 수도 있다.
+// CDEFGABCDEFGAB
+// [3차] 방금그곡
+
+function solution(m, musicinfos) {
+    let titles = [];
+    for (let i = 0; i < musicinfos.length; i++) {
+        let [str, end, musicTitle, musicInfo] = musicinfos[i].split(',');
+        let times = 0;
+        let music;
+
+        musicInfo = musicInfo.match(/([a-zA-z]#)|([A-Z]|)/g).filter(v => v);
+        str = Number(str.replace(":", ""));
+        end = Number(end.replace(":", ""));
+        for (let j = str; j <= end; j++) {
+            if (times === musicInfo.length) times = 0;
+            if (!music) music = musicInfo[times];
+            else music += musicInfo[times];
+
+            times++;
+        }
+        if (music.indexOf(m) !== -1) {
+            titles.push({title: musicTitle, idx: music.indexOf(m)});
+        }
     }
-    return gameBoard.flatMap(v => v).filter(v => !v).length
+    return !titles.length ? '(None)' : titles.reduce((acc,curr)=> {
+        if (acc.idx < curr.idx) {
+            return acc;
+        } else {
+            return curr;
+        }
+    }).title;
 }
 
-const findBingo = (board, width, height) => {
-    const binggo = Array.from({length: height}, () => Array(height).fill(false));
-    for (let i = 1; i < height; i++) {
-        for (let j = 1; j < width; j++) {
-            if (!board[i][j] || !board[i][j - 1] || !board[i - 1][j - 1] || !board[i - 1][j]) continue;
-            if (board[i][j] === board[i][j - 1] && board[i][j] === board[i - 1][j - 1] && board[i][j] === board[i - 1][j]) {
-                binggo[i][j] = true;
-                binggo[i][j - 1] = true;
-                binggo[i - 1][j - 1] = true;
-                binggo[i - 1][j] = true;
-            }
-        }
-    }
-    makeBoard(board, binggo);
-};
+console.log(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]));
+console.log(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]));
+console.log(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]));
 
 
-const makeBoard = (board, binggo) => {
-    binggo.forEach((binggo, i) => {
-        for (let j = 0; j < binggo.length; j++) {
-            if (binggo[j]) {
-                board[i][j] = null;
-            }
-        }
-    });
-    for (let i = 0; i < board.length - 1; i++) {
-        // null 처리 후에 한칸 식 이동하는 것이 아니라 전부 옮겨 보자
-        board[i].forEach((item, idx) => {
-            if (!item) return;
-            if (!board[i + 1][idx]) [board[i + 1][idx], board[i][idx]] = [item, null];
-        });
-    }
-};
+// console.log(Number("12:00".replace(":","")))
+// console.log("CC#BCC#BCC#BCC#BCC#BCC#BCC#BCC#BCC#BCC#BC".indexOf("CC#BCC#BCC#BCC#B"))
+// console.log("CC#BCC#BCC#B".indexOf("CC#BCC#BCC#BCC#B"))
 
-// console.log(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]));
-// console.log(solution(6, 6, ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]));
-console.log(solution(5, 6, ["AAAAAA",
-    "BBAATB",
-    "BBAATB",
-    "JJJTAA",
-    "JJJTAA"]));
+console.log("C#DEFGABC#DEFGABC#".indexOf("ABC"));
+console.log("ABCDEF".indexOf("ABC"));
 
 
-// 채점 결과
-// 정확성: 81.8
-// 합계: 81.8 / 100.0
+// 정확성: 86.7
+// 합계: 86.7 / 100.0

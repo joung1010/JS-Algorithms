3e179c07bd95b97bfcd086bca5d1e712c49b10d7
mason park <joung4342@gmail.com>
2023-07-18 21:57:29 +0900

    [3차] 압축 문제풀일 3글자가 사전에 존재하는 케이스 고려해서 다시풀이

--- Quiz/0.test.js
+++ Quiz/0.test.js
@@ -1,53 +1,51 @@
-// 라디오에서 방금 나왔던 음악이 무슨 음악인지 궁금해질 때가 많다
-// 네오는 자신이 기억한 멜로디를 가지고 방금그곡을 이용해 음악을 찾는다
-// 디오 방송에서는 한 음악을 반복해서 재생할 때도 있어서 네오가 기억하고 있는
-// 멜로디는 음악 끝부분과 처음 부분이 이어서 재생된 멜로디일 수도 있다.
-// 반대로, 한 음악을 중간에 끊을 경우 원본 음악에는 네오가 기억한 멜로디가 들어있다 해도 그 곡이 네오가 들은 곡이 아닐 수도 있다.
-// CDEFGABCDEFGAB
-// [3차] 방금그곡
+// LZW 압축은 다음 과정을 거친다.
 
-function solution(m, musicinfos) {
-    let titles = [];
-    for (let i = 0; i < musicinfos.length; i++) {
-        let [str, end, musicTitle, musicInfo] = musicinfos[i].split(',');
-        let times = 0;
-        let music;
+// 길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
+// 사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
+// w에 해당하는 사전의 색인 번호를 출력하고, 입력에서 w를 제거한다.
+// 입력에서 처리되지 않은 다음 글자가 남아있다면(c), w+c에 해당하는 단어를 사전에 등록한다.
+// 단계 2로 돌아간다.
+// 압축 알고리즘이 영문 대문자만 처리한다고 할 때, 사전은 다음과 같이 초기화된다. 사전의 색인 번호는 정수값으로 주어지며, 1부터 시작한다고 하자.
 
-        musicInfo = musicInfo.match(/([a-zA-z]#)|([A-Z]|)/g).filter(v => v);
-        str = Number(str.replace(":", ""));
-        end = Number(end.replace(":", ""));
-        for (let j = str; j <= end; j++) {
-            if (times === musicInfo.length) times = 0;
-            if (!music) music = musicInfo[times];
-            else music += musicInfo[times];
+/*[...msg].reduce((acc, curr,idx,msgArr) => {
+    arr.includes(acc) && result.push(arr.indexOf(acc) + 1);
+    const char = acc + curr;
 
-            times++;
-        }
-        if (music.indexOf(m) !== -1) {
-            titles.push({title: musicTitle, idx: music.indexOf(m)});
+    if (arr.includes(char)) {
+        return char;
+    } else {
+        arr.push(char);
+        idx === msg.length - 1 && result.push(arr.indexOf(curr) + 1);
+        return arr.includes(curr + msgArr[idx + 1]) ? curr + msgArr[idx + 1] : curr;
+    }
+
+});*/
+function solution(msg) {
+    const arr = Array.from({length: 26}, (v, i) => String.fromCharCode(i + 65));
+    const result = [];
+    const msgArr = msg.split("");
+    while (msgArr.length) {
+        let char = msgArr.shift();
+        let char2 = char + msgArr[0];
+
+        if (arr.includes(char)) {
+            if (arr.includes(char2)) {
+                msgArr.shift();
+                result.push(arr.indexOf(char2) + 1);
+            } else {
+                result.push(arr.indexOf(char) + 1);
+            }
+            arr.push(char2);
         }
     }
-    return !titles.length ? '(None)' : titles.reduce((acc,curr)=> {
-        if (acc.idx < curr.idx) {
-            return acc;
-        } else {
-            return curr;
-        }
-    }).title;
+
+    return result;
 }
 
-console.log(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]));
-console.log(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]));
-console.log(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]));
-
-
-// console.log(Number("12:00".replace(":","")))
-// console.log("CC#BCC#BCC#BCC#BCC#BCC#BCC#BCC#BCC#BCC#BC".indexOf("CC#BCC#BCC#BCC#B"))
-// console.log("CC#BCC#BCC#B".indexOf("CC#BCC#BCC#BCC#B"))
-
-console.log("C#DEFGABC#DEFGABC#".indexOf("ABC"));
-console.log("ABCDEF".indexOf("ABC"));
-
-
-// 정확성: 86.7
-// 합계: 86.7 / 100.0
+// console.log(solution("KAKAO"));
+console.log(solution("TOBEORNOTTOBEORTOBEORNOT"));
+//[
+//   20, 15,  2,  5, 15, 18, 14,
+//   15, 20, 27, 29, 31,  여기부터 오류  27, 29,
+//   31, 33, 20
+// ]
